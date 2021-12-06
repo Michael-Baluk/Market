added6708d4f14b4166488b215f31e96c2eca8f1
Michael-Baluk <maikelbaluk@gmail.com>
2021-12-06 17:27:47 -0300

    update

--- info_market/info_market/views.py
+++ info_market/info_market/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from apps.productos.models import Producto
+
 
 def inicio(request):
-    return render(request, 'inicio.html')
+
+    productos = Producto.objects.all()
+    usuario = {
+        'nombre': 'Maikel',
+        'apellido': 'Baluk'
+    }
+
+    context = {
+        'usuario': usuario,
+        'productos': productos ,
+    }
+
+    return render(request, 'inicio.html', context)
